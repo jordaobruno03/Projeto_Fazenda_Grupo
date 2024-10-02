00507791a44b33a94d7da8e8fe354db884ebe6cb
HenriqueRitto <168286260+HenriqueRitto@users.noreply.github.com>
2024-10-02 14:11:43 -0300

    Add files via upload

---
+++ TelaLoginMobile/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TelaLogin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Bem vindo!!", "Bem vindo a nossa Fazenda Urbana!!", "Ok");
+        }
+    }
+}
